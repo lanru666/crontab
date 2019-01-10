b175628237a1f03bc26af67e204e13065f5241e6
17381792 <17381792@qq.com>
2019-01-10 15:53:21 +0800

    add main

--- common/Protocol.go
+++ common/Protocol.go
@@ -1,8 +1,32 @@
 package common
 
+import "encoding/json"
+
 // 定时任务
 type Job struct {
-	Name     string `json:"name"` // 任务名
-	Command  string `json:"command"` // shell命令
+	Name     string `json:"name"`     // 任务名
+	Command  string `json:"command"`  // shell命令
 	CronExpr string `json:"cronExpr"` // cron表达式
+}
+
+//HTTP接口应答
+type Response struct {
+	Errno int         `json:"errorno"`
+	Msg   string      `json:"msg"`
+	data  interface{} `json:"data"`
+}
+
+// 应答方法
+func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err error) {
+	// 1、定义一个response
+	var (
+		response Response
+	)
+	response.Errno = errno
+	response.Msg = msg
+	response.data = data
+	// 2、序列化json
+	resp, err = json.Marshal(response)
+	
+	return
 }
--- master/ApiServer.go
+++ master/ApiServer.go
@@ -25,6 +25,8 @@ func handleJobSave(resp http.ResponseWriter, req *http.Request) {
 		err     error
 		postJob string
 		job     common.Job
+		oldJob  *common.Job
+		bytes   []byte
 	)
 	//1、解析POST表单
 	if err = req.ParseForm(); err != nil {
@@ -36,7 +38,20 @@ func handleJobSave(resp http.ResponseWriter, req *http.Request) {
 	if err = json.Unmarshal([]byte(postJob), &job); err != nil {
 		goto ERR
 	}
+	//4、保存到etcd
+	if oldJob, err = G_jobMgr.SaveJob(&job); err != nil {
+		goto ERR
+	}
+	// 5、返回正常应答
+	if bytes, err = common.BuildResponse(0, "success", oldJob); err != nil {
+		resp.Write(bytes)
+	}
+	return
 ERR:
+	// 6、返回异常应答
+	if bytes, err = common.BuildResponse(-1, err.Error(), nil); err != nil {
+		resp.Write(bytes)
+	}
 }
 
 //初始化服务
