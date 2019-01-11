109aaef0eb195f67e6d3c032ebdf5ef67766b602
17381792 <17381792@qq.com>
2019-01-11 11:43:36 +0800

    add main

--- master/ApiServer.go
+++ master/ApiServer.go
@@ -56,6 +56,43 @@ ERR:
 	}
 }
 
+// 删除任务接口
+// POST /job/delete  name=job1
+func handleJobDelete(resp http.ResponseWriter, req *http.Request) {
+	fmt.Println("handleJobSave")
+	var (
+		err     error
+		postJob string
+		job     common.Job
+		oldJob  *common.Job
+		bytes   []byte
+	)
+	//1、解析POST表单
+	if err = req.ParseForm(); err != nil {
+		goto ERR
+	}
+	//2、取表单的job字段
+	postJob = req.PostForm.Get("job")
+	//3、反序列化job
+	if err = json.Unmarshal([]byte(postJob), &job); err != nil {
+		goto ERR
+	}
+	//4、保存到etcd
+	if oldJob, err = G_jobMgr.SaveJob(&job); err != nil {
+		goto ERR
+	}
+	// 5、返回正常应答
+	if bytes, err = common.BuildResponse(0, "success", oldJob); err == nil {
+		resp.Write(bytes)
+	}
+	return
+ERR:
+	// 6、返回异常应答
+	if bytes, err = common.BuildResponse(-1, err.Error(), nil); err == nil {
+		resp.Write(bytes)
+	}
+}
+
 //初始化服务
 func InitApiServer() (err error) {
 	var (
@@ -66,6 +103,8 @@ func InitApiServer() (err error) {
 	//配置路由
 	mux = http.NewServeMux()
 	mux.HandleFunc("/job/save", handleJobSave)
+	mux.HandleFunc("/job/delete", handleJobDelete)
+	
 	//启动TCP监听
 	if listener, err = net.Listen("tcp", ":"+strconv.Itoa(G_config.ApiPort)); err != nil {
 		return
