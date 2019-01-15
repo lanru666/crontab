9f166b13352544788575185a95976e6b9d187813
17381792 <17381792@qq.com>
2019-01-15 11:27:24 +0800

    add main

--- worker/Executor.go
+++ worker/Executor.go
@@ -1,6 +1,10 @@
 package worker
 
-import "github.com/lanru666/crontab/common"
+import (
+	"context"
+	"github.com/lanru666/crontab/common"
+	"os/exec"
+)
 
 // 任务执行器
 type Executor struct {
@@ -12,10 +16,25 @@ var (
 
 //执行一个任务
 func (executor *Executor) ExecuteJob(info *common.JobExecuteInfo) {
-
+	go func() {
+		var (
+			cmd    *exec.Cmd
+			err    error
+			output []byte
+		)
+		// 执行shell命令
+		cmd = exec.CommandContext(context.TODO(), "/bin/bash", "-c", info.Job.Command)
+		// 执行并捕获输出
+		output, err = cmd.CombinedOutput()
+		//任务执行完成后，把执行的结果返回给Scheduler
+		
+	}()
 }
 
 //初始化执行器
 func InitExecutor() (err error) {
+	G_executor = &Executor{
+	
+	}
 	return
 }
