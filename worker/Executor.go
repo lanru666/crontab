e1a6829cf0c3d67d86ac072f022dad58f3605fdf
17381792 <17381792@qq.com>
2019-01-15 22:07:44 +0800

    add main

--- worker/Executor.go
+++ worker/Executor.go
@@ -34,16 +34,21 @@ func (executor *Executor) ExecuteJob(info *common.JobExecuteInfo) {
 		jobLock = G_jobMgr.CreateJobLock(info.Job.Name)
 		// 记录任务开始时间
 		result.StartTime = time.Now()
-		// 执行shell命令
-		cmd = exec.CommandContext(context.TODO(), "/bin/bash", "-c", info.Job.Command)
-		// 执行并捕获输出
-		output, err = cmd.CombinedOutput()
-		// 记录任务结束时间
-		result.EndTime = time.Now()
-		result.OutPut = output
-		result.Err = err
-		//任务执行完成后，把执行的结果返回给Scheduler,Scheduler会从ExecutingTable中删除掉执行记录
-		G_scheduler.PushJobResult(result)
+		if err = jobLock.TryLock(); err != nil { //上锁失败
+			result.Err = err
+			result.EndTime = time.Now()
+		} else {
+			// 执行shell命令
+			cmd = exec.CommandContext(context.TODO(), "/bin/bash", "-c", info.Job.Command)
+			// 执行并捕获输出
+			output, err = cmd.CombinedOutput()
+			// 记录任务结束时间
+			result.EndTime = time.Now()
+			result.OutPut = output
+			result.Err = err
+			//任务执行完成后，把执行的结果返回给Scheduler,Scheduler会从ExecutingTable中删除掉执行记录
+			G_scheduler.PushJobResult(result)
+		}
 	}()
 }
 
